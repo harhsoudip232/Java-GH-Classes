e6e9f895934a802b123942e4dd251ee3d5259cd7
harhsoudip10 <soudipharh10@gmail.com>
2026-07-25 01:05:01 +0530

    add this demo programs

---
+++ Demo Programs/addNum.java
@@ -0,0 +1,9 @@
+public class addNum {
+    public static void main (String args []) {
+        System.out.print("The Sum of these numbers: ");
+        int a = 12;
+        int b = 22;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
